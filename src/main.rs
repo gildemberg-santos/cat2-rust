dd20a9c586a9bff6503ab130c9ad07df6e772d37
gildemberg-santos <gildemberg.santos@gmail.com>
2023-01-03 14:48:55 -0300

    Ler arquivo de texto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename: &String = &args[1];
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("{}", contents);
 }
